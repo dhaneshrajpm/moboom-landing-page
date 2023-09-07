a2108c91db919dcaed853183ed20dd7c0e228ec8
dhaneshrajpm <dhaneshraj2311@gmail.com>
2023-09-07 09:30:55 +0530

    Fav icon added

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Rate } from 'antd';
-import { AiOutlineHeart } from 'react-icons/ai';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 
 import styles from './ProductCard.module.scss';
 
 const ProductCard = ({ title, thumbnail, price, rating, description }) => {
+  const [fav, setFav] = useState(false);
+
+  const onClick = useCallback(() => {
+    setFav(!fav)
+  }, [fav, setFav]);
+
   return (
     <div className={styles.container}>
       <img className={styles.image} src={thumbnail} alt='OOPS!, failed to fetch' />
-      <span className={styles.favItem}>< AiOutlineHeart /></span>
+      <span role='button' onClick={onClick} className={styles.favItem}>{fav ? <AiFillHeart className={styles.fillHeart} /> : < AiOutlineHeart className={styles.outlineHeart} />}</span>
       <div className={styles.productDetails}>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.description}>Product description: {description}</p>
